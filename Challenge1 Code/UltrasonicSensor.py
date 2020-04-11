03d8206a6b1e1908cc67af5ab65204608b107ad1
sdubois18 <samdubois18@icloud.com>
2020-04-11 19:25:08 -0400

    Created new Constants file and Sensor file

---
+++ Challenge1 Code/UltrasonicSensor.py
@@ -0,0 +1,40 @@
+# UMass Robotics Team 
+# Challenge01 Main Project File
+#
+# UltrasonicSensor.py
+#
+# Created by Samuel DuBois and Andrew Tran
+# Created on April 11, 2020
+
+import RPi.GPIO as gpio
+import time
+import Constants
+
+# Description: gets data from the Ultrasonic Sensor and returns an int
+def getDistanceFromSensor():
+
+	# Set the TRIG to false in order to settle sensor
+	gpio.output(Constants.TRIG, False)
+	time.sleep(2)
+
+	# Trigger the TRIG to start the sensor
+	gpio.output(Constants.TRIG, True)
+	time.sleep(0.0001)
+	gpio.output(Constants.TRIG, False)
+
+	# Record the start and end of the pulse to determine the time it took to come back
+	while gpio.input(ECHO) == 0:
+		pulse_start = time.time()
+
+	while gpio.input(ECHO) == 1:
+		pulse_end = time.time()
+
+	# Mathemetically calculate distance
+	pulse_duration = pulse_end - pulse_start
+
+	distance = pulse_duration * 17150
+
+	distance = round(distance, 2)
+
+	gpio.cleanup()
+	return distance
--- Challenge1 Code/main.py
+++ Challenge1 Code/main.py
@@ -6,23 +6,9 @@
 # Created by Samuel DuBois and Andrew Tran
 # Created on April 11, 2020
 
-import RPi.GPIO as gpio 
+import RPi.GPIO as gpio # GPIO Library
 import time
-
-# MARK: Constants and Variables
-
-# H-Bridge / Motor Controller
-IN1 = 7
-IN2 = 11
-IN3 = 13
-IN4 = 15
-
-# HC-SR04 Ultrasonic Sensor
-TRIG = 16
-ECHO = 18
-
-# Status LED
-LED = 22
+import Constants 		# Constants Python File
 
 # MARK: Functions
 
@@ -32,19 +18,24 @@ def setupPins():
 
 	# H-Bridge / Motor Controller Pins
 	gpio.setmode(gpio.BOARD)
-	gpio.setup(IN1, gpio.OUT)
-	gpio.setup(IN2, gpio.OUT)
-	gpio.setup(IN3, gpio.OUT)
-	gpio.setup(IN4, gpio.OUT)
+	gpio.setup(Constants.IN1, gpio.OUT)
+	gpio.setup(Constants.IN2, gpio.OUT)
+	gpio.setup(Constants.IN3, gpio.OUT)
+	gpio.setup(Constants.IN4, gpio.OUT)
 
 	# HC-SR04 Ultrasonic Sensor Pins
-	gpio.setup(TRIG, gpio.OUT)
-	gpio.setup(ECHO, gpio.IN)
+	gpio.setup(Constants.TRIG, gpio.OUT)
+	gpio.setup(Constants.ECHO, gpio.IN)
 
 	# LED Status Pin
-	gpio.setup(LED, gpio.OUT)
+	gpio.setup(Constants.LED, gpio.OUT)
 
+# Description: Main Method for executing main code
+# Main Code
 if __name__ == "__main__":
 	setupPins()
 	print("Hello World")
+
+
+
 
