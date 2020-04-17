5c3295c7e166c46268b3c72d268e3382b0eb5748
Andrew Tran <andtran@umass.edu>
2020-04-16 23:39:52 -0400

    Changed MotorControls.py to use pwm. Duty Cycle = 50% Frequency = 100Hz TODO: Need to actually test these methods to make sure it works
    In each function; initialize the pins to be pwm at 100Hz, then sends signals accordingly

--- Challenge1 Code/MotorControls.py
+++ Challenge1 Code/MotorControls.py
@@ -14,51 +14,87 @@ import Constants
 Motor controls for the robot to be used in tandem with UltrasonicSensor.py to create
 basic algorithm for movement
 
-IN1 and IN2 control left side of the bot
-IN3 and IN4 control right side of the bot
-
-False-False --> off
-False-True --> forward
-True-False --> reverse
+PWM used to slow down dc motors on cars.
+Duty cycle = 50%
+Frequency = 100Hz
 """
 
+# TODO: Still need to test these methods to make sure bot doesn't go too fast
 
 def stop():
-    gpio.output(Constants.IN1, False)
-    gpio.output(Constants.IN2, False)
-    gpio.output(Constants.IN3, False)
-    gpio.output(Constants.IN4, False)
+    # make output signals pwm
+    # change second argument to change frequency of signal
+    pwm1 = gpio.pwm(Constants.IN1, 100)
+    pwm2 = gpio.pwm(Constants.IN2, 100)
+    pwm3 = gpio.pwm(Constants.IN3, 100)
+    pwm4 = gpio.pwm(Constants.IN4, 100)
+
+    pwm1.stop()
+    pwm2.stop()
+    pwm3.stop()
+    pwm4.stop()
 
 
 def forward():
-    gpio.output(Constants.IN1, False)
-    gpio.output(Constants.IN2, True)
-    gpio.output(Constants.IN3, True)
-    gpio.output(Constants.IN4, False)
+    # make output signals pwm
+    # change second argument to change frequency of signal
+    pwm1 = gpio.pwm(Constants.IN1, 100)
+    pwm2 = gpio.pwm(Constants.IN2, 100)
+    pwm3 = gpio.pwm(Constants.IN3, 100)
+    pwm4 = gpio.pwm(Constants.IN4, 100)
+
+    pwm1.stop()
+    pwm2.start(50)
+    pwm3.start(50)
+    pwm4.stop()
 
 
 def reverse():
-    gpio.output(Constants.IN1, True)
-    gpio.output(Constants.IN2, False)
-    gpio.output(Constants.IN3, False)
-    gpio.output(Constants.IN4, True)
+    # make output signals pwm
+    # change second argument to change frequency of signal
+    pwm1 = gpio.pwm(Constants.IN1, 100)
+    pwm2 = gpio.pwm(Constants.IN2, 100)
+    pwm3 = gpio.pwm(Constants.IN3, 100)
+    pwm4 = gpio.pwm(Constants.IN4, 100)
+
+    pwm1.start(50)
+    pwm2.stop()
+    pwm3.start(50)
+    pwm4.stop()
+
 
 # function rotate_left/right(time) --> void
 # must have some parameter to let car know how long to rotate for
 # will have to sample timings to understand math behind it.
 def rotate_left(tp):
-    gpio.output(Constants.IN1, False)
-    gpio.output(Constants.IN2, True)
-    gpio.output(Constants.IN3, False)
-    gpio.output(Constants.IN4, True)
+    # make output signals pwm
+    # change second argument to change frequency of signal
+    pwm1 = gpio.pwm(Constants.IN1, 100)
+    pwm2 = gpio.pwm(Constants.IN2, 100)
+    pwm3 = gpio.pwm(Constants.IN3, 100)
+    pwm4 = gpio.pwm(Constants.IN4, 100)
+
+    pwm1.stop()
+    pwm2.start(50)
+    pwm3.stop()
+    pwm4.start(50)
 
     time.sleep(tp)
+    stop()
 
 
 def rotate_right(tp):
-    gpio.output(Constants.IN1, True)
-    gpio.output(Constants.IN2, False)
-    gpio.output(Constants.IN3, True)
-    gpio.output(Constants.IN4, False)
+    # make output signals pwm
+    # change second argument to change frequency of signal
+    pwm1 = gpio.pwm(Constants.IN1, 100)
+    pwm2 = gpio.pwm(Constants.IN2, 100)
+    pwm3 = gpio.pwm(Constants.IN3, 100)
+    pwm4 = gpio.pwm(Constants.IN4, 100)
+
+    pwm1.start(50)
+    pwm2.stop()
+    pwm3.start(50)
+    pwm4.stop()
 
     time.sleep(tp)
+    stop()
