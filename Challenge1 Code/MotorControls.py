196e06c58cf41e9d20fb389b1f493d0d2eda56e0
Andrew Tran <andtran@umass.edu>
2020-04-17 14:56:40 -0400

    fixed imports on MotorControl.py

--- Challenge1 Code/MotorControls.py
+++ Challenge1 Code/MotorControls.py
@@ -22,79 +22,79 @@ Frequency = Constants.FREQHz
 # TODO: Still need to test these methods to make sure bot doesn't go too fast
 
 def stop():
-    # make output signals pwm
+    # make output signals PWM
     # change second argument to change frequency of signal
-    pwm1 = gpio.pwm(Constants.IN1, Constants.FREQ)
-    pwm2 = gpio.pwm(Constants.IN2, Constants.FREQ)
-    pwm3 = gpio.pwm(Constants.IN3, Constants.FREQ)
-    pwm4 = gpio.pwm(Constants.IN4, Constants.FREQ)
+    PWM1 = gpio.PWM(Constants.IN1, Constants.FREQ)
+    PWM2 = gpio.PWM(Constants.IN2, Constants.FREQ)
+    PWM3 = gpio.PWM(Constants.IN3, Constants.FREQ)
+    PWM4 = gpio.PWM(Constants.IN4, Constants.FREQ)
 
-    pwm1.stop()
-    pwm2.stop()
-    pwm3.stop()
-    pwm4.stop()
+    PWM1.stop()
+    PWM2.stop()
+    PWM3.stop()
+    PWM4.stop()
 
 
 def forward():
-    # make output signals pwm
+    # make output signals PWM
     # change second argument to change frequency of signal
-    pwm1 = gpio.pwm(Constants.IN1, Constants.FREQ)
-    pwm2 = gpio.pwm(Constants.IN2, Constants.FREQ)
-    pwm3 = gpio.pwm(Constants.IN3, Constants.FREQ)
-    pwm4 = gpio.pwm(Constants.IN4, Constants.FREQ)
+    PWM1 = gpio.PWM(Constants.IN1, Constants.FREQ)
+    PWM2 = gpio.PWM(Constants.IN2, Constants.FREQ)
+    PWM3 = gpio.PWM(Constants.IN3, Constants.FREQ)
+    PWM4 = gpio.PWM(Constants.IN4, Constants.FREQ)
 
-    pwm1.stop()
-    pwm2.start(Constants.DUTYCYCLE)
-    pwm3.start(Constants.DUTYCYCLE)
-    pwm4.stop()
+    PWM1.stop()
+    PWM2.start(Constants.DUTYCYCLE)
+    PWM3.start(Constants.DUTYCYCLE)
+    PWM4.stop()
 
 
 def reverse():
-    # make output signals pwm
+    # make output signals PWM
     # change second argument to change frequency of signal
-    pwm1 = gpio.pwm(Constants.IN1, Constants.FREQ)
-    pwm2 = gpio.pwm(Constants.IN2, Constants.FREQ)
-    pwm3 = gpio.pwm(Constants.IN3, Constants.FREQ)
-    pwm4 = gpio.pwm(Constants.IN4, Constants.FREQ)
+    PWM1 = gpio.PWM(Constants.IN1, Constants.FREQ)
+    PWM2 = gpio.PWM(Constants.IN2, Constants.FREQ)
+    PWM3 = gpio.PWM(Constants.IN3, Constants.FREQ)
+    PWM4 = gpio.PWM(Constants.IN4, Constants.FREQ)
 
-    pwm1.start(Constants.DUTYCYCLE)
-    pwm2.stop()
-    pwm3.start(Constants.DUTYCYCLE)
-    pwm4.stop()
+    PWM1.start(Constants.DUTYCYCLE)
+    PWM2.stop()
+    PWM3.start(Constants.DUTYCYCLE)
+    PWM4.stop()
 
 
 # function rotate_left/right(time) --> void
 # must have some parameter to let car know how long to rotate for
 # will have to sample timings to understand math behind it.
 def rotate_left(tp):
-    # make output signals pwm
+    # make output signals PWM
     # change second argument to change frequency of signal
-    pwm1 = gpio.pwm(Constants.IN1, Constants.FREQ)
-    pwm2 = gpio.pwm(Constants.IN2, Constants.FREQ)
-    pwm3 = gpio.pwm(Constants.IN3, Constants.FREQ)
-    pwm4 = gpio.pwm(Constants.IN4, Constants.FREQ)
+    PWM1 = gpio.PWM(Constants.IN1, Constants.FREQ)
+    PWM2 = gpio.PWM(Constants.IN2, Constants.FREQ)
+    PWM3 = gpio.PWM(Constants.IN3, Constants.FREQ)
+    PWM4 = gpio.PWM(Constants.IN4, Constants.FREQ)
 
-    pwm1.stop()
-    pwm2.start(Constants.DUTYCYCLE)
-    pwm3.stop()
-    pwm4.start(Constants.DUTYCYCLE)
+    PWM1.stop()
+    PWM2.start(Constants.DUTYCYCLE)
+    PWM3.stop()
+    PWM4.start(Constants.DUTYCYCLE)
 
     time.sleep(tp)
     stop()
 
 
 def rotate_right(tp):
-    # make output signals pwm
+    # make output signals PWM
     # change second argument to change frequency of signal
-    pwm1 = gpio.pwm(Constants.IN1, Constants.FREQ)
-    pwm2 = gpio.pwm(Constants.IN2, Constants.FREQ)
-    pwm3 = gpio.pwm(Constants.IN3, Constants.FREQ)
-    pwm4 = gpio.pwm(Constants.IN4, Constants.FREQ)
+    PWM1 = gpio.PWM(Constants.IN1, Constants.FREQ)
+    PWM2 = gpio.PWM(Constants.IN2, Constants.FREQ)
+    PWM3 = gpio.PWM(Constants.IN3, Constants.FREQ)
+    PWM4 = gpio.PWM(Constants.IN4, Constants.FREQ)
 
-    pwm1.start(Constants.DUTYCYCLE)
-    pwm2.stop()
-    pwm3.start(Constants.DUTYCYCLE)
-    pwm4.stop()
+    PWM1.start(Constants.DUTYCYCLE)
+    PWM2.stop()
+    PWM3.start(Constants.DUTYCYCLE)
+    PWM4.stop()
 
     time.sleep(tp)
     stop()
--- Challenge1 Code/main.py
+++ Challenge1 Code/main.py
@@ -48,11 +48,11 @@ if __name__ == "__main__":
 		if distance > 40:
 			print("Moving Forward")
 			mc.forward()
-			time.sleep(0.75)
+			time.sleep(0.5)
 			gpio.cleanup()
 		else:
 			print("Rotating Right")
-			mc.rotate_right(0.75)
+			mc.rotate_right(0.5)
 			gpio.cleanup()
 
 
