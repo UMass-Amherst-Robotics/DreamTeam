c66ab50452a9816574a3f977739f935c97a3d370
sdubois18 <samdubois18@icloud.com>
2020-04-19 13:41:39 -0400

    small change

--- Challenge1 Code/main.py
+++ Challenge1 Code/main.py
@@ -72,14 +72,16 @@ if __name__ == "__main__":
 
 		# Read the distance and check to see
 		if distance > 40:
-			mc.forwards(0.030, 1000, 77)
+			mc.forwards(0.030, 5000, 77)
 			print("Moving Forward")
 		else:
-			mc.rotateRight(0.030, 1000, 77)
+			mc.rotateRight(0.030, 5000, 77)
 			print("Rotating Right")
 
 		if int(distance - previousDistanceReading) < 2:
 			numOfSameDistanceReadings += 1
+
+		previousDistanceReading = distance
 
 		intervalsUntilCompletion += 1
 
