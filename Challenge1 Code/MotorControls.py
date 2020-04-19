b13185fb7551fbeb10322d278589e1b4f5229912
sdubois18 <samdubois18@icloud.com>
2020-04-19 14:33:40 -0400

    typo

--- Challenge1 Code/MotorControls.py
+++ Challenge1 Code/MotorControls.py
@@ -31,7 +31,7 @@ motors = getMotorsForPWM()
 
 def getMotorsForPWM():
     gpio.setmode(gpio.BCM)
-	motors = [gpio.PWM(Constants.IN1, 5000), gpio.PWM(Constants.IN2, 5000), gpio.PWM(Constants.IN3, 5000), gpio.PWM(Constants.IN4, 5000)]
+    motors = [gpio.PWM(Constants.IN1, 5000), gpio.PWM(Constants.IN2, 5000), gpio.PWM(Constants.IN3, 5000), gpio.PWM(Constants.IN4, 5000)]
     for motor in motors:
         motor.start(0)
 
